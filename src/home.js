d5ae3be96f58305ed8d8e0138b322cdfbd520178
Soham <soham5959595@gmail.com>
2018-08-09 01:00:35 +0530

    fetchData callback function added. Coins retrieved on multiple pages. Button logic needs to be fixed.

--- src/home.js
+++ src/home.js
@@ -1,62 +1,85 @@
 import React from 'react';
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
-import '../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
 class HomePage extends React.Component {
     constructor() {
         super();
         this.state = {
-            listing: [],
+            currentPage: 0,
+            data: [],
+            isLoading: false
         };
     }
 
+    fetchData(page) {
+        this.setState({ isLoading: true });
+        //Set it to the amount of records you want per page
+        const rowsPerPage = 100;
+        const start = this.state.currentPage * rowsPerPage;
+        fetch(`https://api.coinmarketcap.com/v2/ticker/?convert=USD&structure=array&start=${start}`)
+            .then(data => data.json())
+            .then(({ data }) => {
+                this.setState({
+                    data,
+                    currentPage: page,
+                    isLoading: false
+                })
+            })
+    };
+
     componentDidMount() {
-        fetch('https://api.coinmarketcap.com/v2/ticker/?convert=USD&structure=array')
-            .then(data => {
-                return data.json();
-            }).then(data => {
-            let listing = data.data.map((list) => {
-                return (
-                    <tr key={list.id}>
-                        <td>{list.id}</td>
-                        <td>{list.rank}</td>
-                        <td>{list.name} </td>
-                        <td>{list.quotes? (list.quotes.USD||{}).price||0 : 0}</td>
-                        <td>{list.symbol}</td>
-                        <td>{list.quotes.USD.volume_24h}</td>
-                        <td>{list.quotes.USD.percent_change_24h}%</td>
-                    </tr>
-                )
-            });
-            this.setState({listing: listing});
-            console.log("state", this.state.listing);
-        })
+        this.fetchData(this.state.currentPage)
     }
 
     render() {
+        const { currentPage, data, isLoading } = this.state;
+
+        // Always a good thing to check if the data is loaded
+        if (isLoading)
+            return <div>Loading...</div>;
+
+        // Maybe you'll need a better logic here
+        const prevPage = currentPage === 0 ? 0 : currentPage - 1;
+        const nextPage = currentPage + 1;
+
+        // Implement your rendering logic here
+        const rows = data.map(
+            row =>
+                <tr key={row.id}>
+                    <td>{row.rank}</td>
+                    <td>{row.name}</td>
+                    <td>{row.quotes? (row.quotes.USD||{}).price||0 : 0}</td>
+                    <td>{row.symbol}</td>
+                    <td>{row.quotes.USD.volume_24h}</td>
+                    <td>{row.quotes.USD.percent_change_24h}%</td>
+                </tr>
+        );
+
+        // Render the table
+        // Take a look at the onClick handlers
         return (
             <div>
                 <div className="jumbotron">
                     <div className="container">
                         <h3>Cryptocurrency Statistics</h3>
                         <br/>
-                        <table id="dtBasicExample" className="table table-borderless table-hover">
+                        <table className="table table-borderless table-hover">
                             <thead>
                             <tr>
-                                <th>ID</th>
                                 <th>#</th>
                                 <th>Name</th>
                                 <th>Price</th>
                                 <th>Symbol</th>
-                                <th>Volume</th>
+                                <th>Volume(24h)</th>
                                 <th>Change(24h)</th>
                             </tr>
                             </thead>
                             <tbody>
-                            {this.state.listing}
+                            {rows}
                             </tbody>
                         </table>
                     </div>
+                    <button onClick={() => this.fetchData(prevPage)}>Prev</button>
+                    <button onClick={() => this.fetchData(nextPage)}>Next</button>
                 </div>
             </div>
         );
