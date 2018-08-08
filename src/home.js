cca26a96abbf9a706f14830a54c580b93d4e6b5d
Soham <soham5959595@gmail.com>
2018-08-08 19:10:06 +0530

    Retrieving ticker api using structure array. Nested json objects retrieval working.

--- src/home.js
+++ src/home.js
@@ -9,17 +9,15 @@ class HomePage extends React.Component {
     }
 
     componentDidMount() {
-        fetch('https://api.coinmarketcap.com/v2/listings/')
+        fetch('https://api.coinmarketcap.com/v2/ticker/?convert=USD&limit=10&structure=array')
             .then(data => {
                 return data.json();
             }).then(data => {
                 let listing = data.data.map((list) => {
                     return (
-                        <li key={list.data}>
-                            {list.name}
-                        </li>
+                        <li key={list.id}>{list.rank} {list.quotes? (list.quotes.USD||{}).price||0 : 0} {list.name} {list.symbol} {list.total_supply}</li>
                     )
-                })
+                });
             this.setState({listing: listing});
                 console.log("state", this.state.listing);
         })
@@ -29,7 +27,7 @@ class HomePage extends React.Component {
         return (
             <div>
                 <div className="jumbotron">
-                    <div className="container text-center">
+                    <div className="container">
                         <p>RANDOM</p>
                         {this.state.listing}
                     </div>
