ef1f468267a5118e43f62ce55725fe9c421c5c1a
Soham <soham5959595@gmail.com>
2018-08-19 18:54:54 +0530

    Added bigger logos, minor design changes in specific currency page.

--- src/components/CoinPage/coinPage.js
+++ src/components/CoinPage/coinPage.js
@@ -11,7 +11,7 @@ class CoinPage extends React.Component {
 
     componentDidMount() {
         const {id} = this.props.match.params;
-        fetch("https://api.coinmarketcap.com/v2/ticker/"+id+"/?convert=BTC&structure=array/")
+        fetch("https://api.coinmarketcap.com/v2/ticker/1/?convert=BTC&structure=array/")
             .then(data => {
                 return data.json();
             }).then(data => {
@@ -19,18 +19,18 @@ class CoinPage extends React.Component {
                 return (
                     <div>
                         <div className="row">
-                            <div className="col-md-3">
-                                <img src={'/coinLogos/'+list.website_slug+'.png'}/> {list.name} ({list.symbol})
+                            <div className="col-md-3 slug">
+                                <img className="specImg" src={'/coinSpecificLogos/'+list.website_slug+'.png'}/><span className="specName"> {list.name}</span><span className="specSymbol"> ({list.symbol})</span>
                             </div>
-                            <div className="col-md-9">
+                            <div className="col-md-7">
                                 <div className="row coinRow">
-                                    <div className="col-md-5">
-                                        <span>${list.quotes?(list.quotes.USD||{}).price||0 : 0} USD<span className={list.quotes.USD.percent_change_24h<0?"red":(list.quotes.USD.percent_change_24h>0?"green":"white")}> ({list.quotes.USD.percent_change_24h}%)</span></span>
+                                    <div className="col-md-12">
+                                        <span className="specUSD">${list.quotes?(list.quotes.USD||{}).price||0 : 0}<span className="specUSDspan"> USD</span> <span className={list.quotes.USD.percent_change_24h<0?"red":(list.quotes.USD.percent_change_24h>0?"green":"white")}> ({list.quotes.USD.percent_change_24h}%)</span></span>
                                     </div>
                                 </div>
                                 <div className="row coinRow">
                                     <div className="col-md-5">
-                                        <span>{list.quotes?(list.quotes.BTC||{}).price||0 : 0} BTC<span className={list.quotes.BTC.percent_change_24h<0?"red":(list.quotes.BTC.percent_change_24h>0?"green":"white")}> ({list.quotes.BTC.percent_change_24h}%)</span></span>
+                                        <span className="specBTC">{list.quotes?(list.quotes.BTC||{}).price||0 : 0} BTC<span className={list.quotes.BTC.percent_change_24h<0?"red":(list.quotes.BTC.percent_change_24h>0?"green":"white")}> ({list.quotes.BTC.percent_change_24h}%)</span></span>
                                     </div>
                                 </div>
                                 <div className="row">
@@ -38,16 +38,16 @@ class CoinPage extends React.Component {
                                         <table className="table table-borderless table-hover specificTable">
                                             <thead>
                                             <tr>
-                                                <th>Volume (24h)</th>
                                                 <th>Market Cap</th>
+                                                <th>Volume (24h)</th>
                                                 <th>Circulating Supply</th>
                                                 <th>Max Supply</th>
                                             </tr>
                                             </thead>
                                             <tbody>
                                             <tr>
-                                                <td>${list.quotes.USD.volume_24h} USD <span>{list.quotes.BTC.volume_24h} BTC</span></td>
-                                                <td>${list.quotes.USD.market_cap} USD <span>{list.quotes.BTC.market_cap} BTC</span></td>
+                                                <td>${list.quotes.USD.market_cap} USD <p className="tableBTC">{list.quotes.BTC.market_cap} BTC</p></td>
+                                                <td>${list.quotes.USD.volume_24h} USD <p className="tableBTC">{list.quotes.BTC.volume_24h} BTC</p></td>
                                                 <td>{list.circulating_supply}</td>
                                                 <td>{list.max_supply}</td>
                                             </tr>
@@ -68,7 +68,7 @@ class CoinPage extends React.Component {
     render() {
         return (
             <div>
-                <div className="container coinContainer">
+                <div className="container-fluid coinContainer">
                     {this.state.listing}
                 </div>
             </div>
